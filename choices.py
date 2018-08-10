fea68972ab4e2a18f39915ff2ce1ddbb822afad7
ilya <n1ro0ily@gmail.com>
2018-08-10 10:19:14 +0300

    add cached property and optimize iter

--- choices.py
+++ choices.py
@@ -1,10 +1,23 @@
 from typing import Tuple, Any
 
 
+class CachedClassProperty:
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, instance, owner):
+        if not hasattr(owner, '_tuple'):
+            owner._tuple = self.func(owner)
+        return owner._tuple
+
+
+cached_class_property = CachedClassProperty
+
+
 class Choice(str):
 
     def __new__(cls, value, text):
-        obj = str.__new__(cls, value)
+        obj = super().__new__(cls, value)
         obj.text = text
         return obj
 
@@ -13,11 +26,10 @@ class EnumChoices:
 
     @classmethod
     def iter(cls):
-        for item_name in dir(cls):
-            item = getattr(cls, item_name)
+        for key, item in cls.__dict__.items():
             if isinstance(item, Choice):
                 yield item
 
-    @classmethod
+    @cached_class_property
     def to_tuple(cls) -> Tuple[Tuple[str, Any]]:
         return tuple((str(item), item.text) for item in cls.iter())
